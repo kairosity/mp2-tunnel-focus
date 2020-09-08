8aed5d2f11a3d4be80528d3a648be6e33daf5b37
kairosity <karinafinegan@gmail.com>
2020-09-08 12:33:06 +0000

    Add a List class and remove hard coded tasks from index.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
-// A main task list that should be an array of task Objects
 
-let taskList = [];
+
 let timeSegment = 0;
 
 // A Task Class that has all the properties and methods associated with a task
@@ -9,114 +8,89 @@ class Task {
     constructor(taskDescription){
         this.taskDescription = taskDescription; //obvz - the only essential prop to create a new task obj.
         this.id = null;
-        this.completed = false; //obvz.
+        this.completed = false; 
         this.order = -1; //order of priority in list - will use to structure list order
         this.totalTimeFocusedOnTask = null; //running total of time focused on a specific task
-    }
-
-    //Add all the methods associated with a task here:
-
-    //edit a task
-    edit(){
-        return "This should run a function that edits the task in the DOM."
-    }
-
-    //countdown15 - should this be a method on  task or should it just be its own function and the method is to call that function on a task? Ask Femi?
-    countdown15(){
-        setTimeout(function(){
-            alert("15 Minutes is up! This is an alarm!")
-        }, 900000)
-    }
-    //countdown25 - same as above - I think the method should just call a countdown25 function.
-    countdown25(){
-        setTimeout(function(){
-            alert("15 Minutes is up! This is an alarm!")
-        }, 15000000)
-    }
-    //opentimer - open ended timer applying time to the task - but I need a separate time segment in case the time needs to be deleted / cancelled
-    // before being added - should there be a time segment object? Or just a global variable? probably just need a global var.
-
-    
+    }   
 }
 
+class List {
+    constructor(){
+        this.taskList = [
+            {
+                taskDescription: "Task numero uno",
+                id: null,
+                completed: false,
+                order: -1,
+                totalTimeFocusedOnTask: "20m43s"
+            },
+            {
+                taskDescription: "Task numero duo",
+                id: null,
+                completed: false,
+                order: -1,
+                totalTimeFocusedOnTask: "4hr10m34s"
+            },
+            {
+                taskDescription: "Task numero tres",
+                id: null,
+                completed: false,
+                order: -1,
+                totalTimeFocusedOnTask: "1hr20m04s"
+            }
+        ];
+        this.completedTasks = [];
+        this.buildTaskList(this.taskList) //the method of building a list is automatic when a new list is instantiated.
+    }
 
-//FUNCTIONS TO WRITE
-
-function createNewTask(){
-    let taskDescription = document.querySelector('#new-task-input').value;
-    if (taskDescription) {
-        let newTask = new Task(taskDescription);
-        //add the new task to the array of task objects.
-        newTask.id = taskList.length;
-        taskList.push(newTask);
-        
-        document.getElementById('list').innerHTML +=
-            `<div class="task">
-                <p>0h0m0s</p>
-                <input class="taskCheckbox" type="checkbox" id="task${newTask.id}" name="task${newTask.id}" onclick="toggleTaskCompletedOnTaskObject()">
-                <li>${newTask.taskDescription}</li>
-                <a><i class="fas fa-sort sort-tasks-icon"></i></a>
-                <a><i class="fas fa-ellipsis-v task-options-icon"></i></a>
-            </div>`;
-        document.querySelector('#new-task-input').value = "";
-       
-
+    buildTaskList(arrayOfTaskObjectsToBuildListWith){
+        let taskList = this.taskList;
+        //takes the tasks stored and formats them into a list when window is loaded. 
+        for (let i=0; i<taskList.length; i++){
+            document.getElementById('list').innerHTML +=
+                `<div class="task">
+                    <p>${taskList[i].totalTimeFocusedOnTask}</p>
+                    <input class="taskCheckbox" type="checkbox" id="${taskList[i].id}" name="task-${taskList[i].id}">
+                    <li>${taskList[i].taskDescription}</li>
+                    <a><i class="fas fa-sort sort-tasks-icon"></i></a>
+                    <a><i class="fas fa-ellipsis-v task-options-icon"></i></a>
+                </div>`;
         }
-    
+    }
+
+    addNewTask(){
+        document.getElementById('add-new-task').addEventListener('click', function(){
+            console.log("clicked")
+        })
+    }
 }
 
-//Need a function that listens for a checkbox being ticked and toggle the completed property on the object being checked. 
+let list = new List();
 
 
-    
-  function toggleTaskCompletedOnTaskObject(event){
-      var checkboxes = document.querySelectorAll('.taskCheckbox');
-
-      for (let box of checkboxes){
-          box.addEventListener('change', function(event){
-              if(event.target.checked){
-                  event.target.nextElementSibling.classList.add('completed');
-                  console.log(`${event.target.id} is completed`);
-              } else {
-                  event.target.nextElementSibling.classList.remove('completed');
-                  console.log(`${event.target.id}still needs to be completed!`);
-              }
-          })
-      }
-
-  }
+//     if (!list){
+//         let list = new List();
+//         let taskDescription = document.querySelector('#new-task-input').value;
+//         if (taskDescription) {
+//             let newTask = new Task(taskDescription);
+//             //add the new task to the array of task objects.
+//             newTask.id = taskList.length;
+//             taskList.push(newTask);
             
+//             document.getElementById('list').innerHTML +=
+//                 `<div class="task">
+//                     <p>0h0m0s</p>
+//                     <input class="taskCheckbox" type="checkbox" id="${newTask.id}" name="task-${newTask.id}">
+//                     <li>${newTask.taskDescription}</li>
+//                     <a><i class="fas fa-sort sort-tasks-icon"></i></a>
+//                     <a><i class="fas fa-ellipsis-v task-options-icon"></i></a>
+//                 </div>`;
+//             document.querySelector('#new-task-input').value = "";
+        
 
+//             }
+        
+//     }
 
-
-
-
-//1. createNewTask() --> instantiates a new task object and adds it to the list & saves to local storage.
-
-//2. countDown15 --> starts a countdown timer for 15 mins. 
-
-//3 CountDown25 --> starts a countdown timer for 25 mins.
-
-//4. Pause timer - function to apply to all timers to pause them?
-
-//5. Reset timer.
-
-//6. Save countdown time to total task focus time. --> Applies to either countdown or open ended timer. When the user clicks save or X they are prompted to "Save this time to task(better
-//phrasing"
-
-//7. Take total task time and create pie/bubble charts from it - D3.js? 
-
-
-
-//Should there be a "TIME" object? - no. Maybe just a series of functions that the Task obj calls? 
-
-// Time segment obj - properties:
-// - timeSaved temporarily
-//  countdown 15 method - as time countsdown that time is pushed into the temp.time segment var.
-// countdown 25 - same as above. 
-// save time -- saves time in the temp time segment var into the task time variable of the associated task. 
-// clear time. -- clears out the temp time segment - if the timer is cancelled. or after use. 
-
-
-
-
+//     }
+// })
